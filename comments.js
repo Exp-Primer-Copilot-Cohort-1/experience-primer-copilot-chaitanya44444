f45fdae0652c9c7688e55b673e9d946412b40680
Chaitanya Vaswani <93338022+chaitanya44444@users.noreply.github.com>
2023-12-28 19:23:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+const http = require('http');
+const server = http.createServer((req, res) => {
+    // Handle requests here
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
